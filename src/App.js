7d003b80665ac88db04746a6ae864fab8c552225
tar04 <taras200195@gmail.com>
2022-10-18 15:09:13 +0300

    fixed master branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom";
-
-import {CommentsOfPost, Navigation, PostDetails, PostsOfUser, UserDetails} from "./components";
-import {PostsPage, UsersPage} from "./pages";
-
 
 const App = () => {
     return (
         <div>
-            <Navigation/>
-            <Routes>
-                <Route path={"/users"} element={<UsersPage/>}>
-                    <Route path={":id"} element={<UserDetails/>}>
-                        <Route path={"posts"} element={<PostsOfUser/>}/>
-                    </Route>
-                </Route>
-                <Route path={"/posts"} element={<PostsPage/>}>
-                    <Route path={":id"} element={<PostDetails/>}>
-                        <Route path={"comments"} element={<CommentsOfPost/>}/>
-                    </Route>
-                </Route>
-            </Routes>
+            App
         </div>
     );
 };
