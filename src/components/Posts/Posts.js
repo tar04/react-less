9edcd78d00584aea4daac2b4d2b8a31d6fc30925
tar04 <taras200195@gmail.com>
2022-10-20 19:13:09 +0300

    pushed all the lessons to vcs

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,18 +1,43 @@
 import React, {useEffect, useState} from "react";
+
 import {postService} from "../../service";
 import {Post} from "../Post/Post";
+import {useSearchParams} from "react-router-dom";
 
 const Posts = () => {
 
     const [posts, setPosts] = useState([]);
 
+    const [query, setQuery] = useSearchParams({page: "1"});
+
+    // useEffect(() => {
+    //     setQuery({page: "1"})
+    // }, []);
+
     useEffect(() => {
-        postService.getAll().then(posts => setPosts(posts));
-    }, []);
+        postService.getAll(query.get("page"), 5).then(posts =>setPosts(posts));
+    }, [query]);
+
+    function nextPage() {
+        setQuery({page: (+query.get("page") + 1).toString()})
+
+        //або, щоб зберігались інші параметри, можна написати так
+        // const quer = Object.fromEntries(query.entries());
+        // quer.page++;
+        // setQuery(quer)
+    }
+
+    function prevPage() {
+        setQuery({page: (+query.get("page") - 1).toString()})
+    }
 
     return (
-        <div style={{minWidth:"50%"}}>
+        <div style={{minWidth: "50%"}}>
             {posts.map(post => <Post key={post.id} post={post}/>)}
+            <div>
+                {!!(+query.get("page")-1) && <button onClick={prevPage}>Prev</button>}
+                {!!posts.length && <button onClick={nextPage}>Next</button>}
+            </div>
         </div>
     );
 };
--- src/service/postService.js
+++ src/service/postService.js
@@ -3,7 +3,12 @@ import axiosService from "./axiosService";
 import {urls} from "../config";
 
 export const postService = {
-    getAll: () => axiosService.get(urls.posts).then(({data}) => data),
+    getAll: (page, _limit=2) => axiosService.get(urls.posts, {
+        params: {
+            _start:(page-1)*_limit,
+            _limit
+        }
+    }).then(({data}) => data),
     getById: (id) => axiosService.get(`${urls.posts}/${id}`).then(({data}) => data),
     getCommentsOfPost: (id) => axiosService.get(`${urls.posts}/${id}${urls.comments}`).then(({data}) => data),
 }
